b10c1b5786d968e1b4a9e9fb4d3e23e9ad5ace7e
eptaccio <eptaccio@gmail.com>
2018-04-21 15:51:21 -0300

    add site config

--- config.js
+++ config.js
@@ -6,5 +6,6 @@ const HOME = os.homedir()
 
 module.exports = {
   USER_CONFIG_FILE_PATH: path.join(HOME, CONFIG_FILE_NAME),
-  API_URL: 'http://localhost:3000'
+  API_URL: 'http://localhost:3000',
+  SITE_URL: 'http://loguito.js.org'
 }
